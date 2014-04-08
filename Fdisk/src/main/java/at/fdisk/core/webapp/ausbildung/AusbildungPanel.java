8d3e03e36bd939f2ed25c69bf12441f63e5c0277
StefanReinprecht <stefan.reinprecht@gmail.com>
2014-04-08 09:33:11 +0200

    Das laden aus der Datenbank funktioniert aber nur in Ausbildung

--- Fdisk/src/main/java/at/fdisk/core/webapp/ausbildung/AusbildungPanel.java
+++ Fdisk/src/main/java/at/fdisk/core/webapp/ausbildung/AusbildungPanel.java
@@ -7,15 +7,23 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.basic.MultiLineLabel;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import at.fdisk.core.domain.Ausbildung;
+import at.fdisk.core.repository.AusbildungRepository;
 import at.fdisk.core.webapp.ContentPanel;
 
 public class AusbildungPanel extends ContentPanel {
 	private List<Ausbildung> ausbildungList = new ArrayList<>();
+	
+	@SpringBean
+	private AusbildungRepository ausbilungRepository;
 
 	public AusbildungPanel(String id) {
 		super(id);
+		
+		ausbildungList.addAll(ausbilungRepository.findAll());
+		
 		add(new AusbildungForm("ausbildungForm", ausbildungList));
 
 		add(new PropertyListView<Ausbildung>("ausbildungList", ausbildungList) {
