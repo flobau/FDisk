8ac655ea99469fe41f4c407bbcadf7fd8192ce3b
Florian Bauernhofer <bau14019@spengergasse.at>
2014-04-09 10:51:33 +0200

    modified Panels

--- Fdisk/src/main/java/at/fdisk/core/webapp/ausruestung/AusruestungPanel.java
+++ Fdisk/src/main/java/at/fdisk/core/webapp/ausruestung/AusruestungPanel.java
@@ -6,15 +6,23 @@ import java.util.List;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import at.fdisk.core.domain.Ausruestung;
+import at.fdisk.core.repository.AusruestungRepository;
 import at.fdisk.core.webapp.ContentPanel;
 
 public class AusruestungPanel extends ContentPanel {
 	private List<Ausruestung> ausruestungList = new ArrayList<>();
 	
+	@SpringBean
+	private AusruestungRepository ausrusetungRepository;
+	
 	public AusruestungPanel(String id) {
 		super(id);
+		
+		ausruestungList.addAll(ausrusetungRepository.findAll());
+		
 		add(new AusruestungForm("ausruestungForm", ausruestungList));
 		
 		add(new PropertyListView<Ausruestung>("ausruestungList", ausruestungList) {
--- Fdisk/src/main/java/at/fdisk/core/webapp/feuerwehrauto/FeuerwehrautoPanel.java
+++ Fdisk/src/main/java/at/fdisk/core/webapp/feuerwehrauto/FeuerwehrautoPanel.java
@@ -6,16 +6,24 @@ import java.util.List;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import at.fdisk.core.domain.Ausruestung;
 import at.fdisk.core.domain.Feuerwehrauto;
+import at.fdisk.core.repository.FeuerwehrautoRepository;
 import at.fdisk.core.webapp.ContentPanel;
 
 public class FeuerwehrautoPanel extends ContentPanel {
 	private List<Feuerwehrauto> feuerwehrautoList = new ArrayList<Feuerwehrauto>();
 
+	@SpringBean
+	private FeuerwehrautoRepository feuerwehrRepository;
+	
 	public FeuerwehrautoPanel(String id) {
 		super(id);
+		
+		feuerwehrautoList.addAll(feuerwehrRepository.findAll());
+		
 		add(new FeuerwehrautoForm("feuerwehrautoForm", feuerwehrautoList));
 
 		add(new PropertyListView<Feuerwehrauto>("feuerwehrautoList",
